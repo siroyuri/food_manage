6ef316ec978bb29e0e59856d0436fc299d9af726
siroyuri <siroyuri_1110@yahoo.co.jp>
2022-02-01 12:49:28 +0900

    【テストコード】usersテーブルのテストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録に成功する' do
+      it '正しい値を全て入力していれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'passwordは6文字以上であれば登録できる' do
+        @user.password = 123456
+        @user.password_confirmation = 123456
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録に失敗する' do
+      it 'nameが空では登録できない' do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it '重複したemailが存在していると登録できない' do
+        @user.save
+        another_user = FactoryBot.create(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it 'emailは@を含んでいないと登録できない' do
+        @user.email = 'testtest'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordは5文字以下では登録できない' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it 'passwordとpassword_confirmationが不一致だと登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '654321'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
+  end
+end
+
+# bundle exec rspec spec/models/user_spec.rb
