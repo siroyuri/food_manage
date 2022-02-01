a3fb2268153582bed0728334999aa531ba503cb8
siroyuri <siroyuri_1110@yahoo.co.jp>
2022-02-01 10:33:07 +0900

    未ログインユーザーはログインページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @categories = Category.where(user_id: current_user.id).order("category_list_id ASC")
   end
